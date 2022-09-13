80d77222d854174008b3a0818930a1099ee248b9
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-13 11:03:37 -0700

    update to readme

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const ejsLayouts = require('express-ejs-layouts');
+const app = express();
+const axios = require('axios')
+
+// Sets EJS as the view engine
+app.set('view engine', 'ejs');
+// Specifies the location of the static assets folder
+app.use(express.static('static'));
+// Sets up body-parser for parsing form data
+app.use(express.urlencoded({ extended: false }));
+// Enables EJS Layouts middleware
+app.use(ejsLayouts);
+
+// Adds some logging to each request
+app.use(require('morgan')('dev'));
+
+// Routes
+app.get('/', function(req, res) {
+  res.render('index.ejs')
+})
+
+
 var myHeaders = new Headers();
 myHeaders.append("x-rapidapi-key", "adc5f02acfmsha3cd71d94f1b43fp1bebf1jsncd36e7ddb757");
 myHeaders.append("x-rapidapi-host", "v1.hockey.api-sports.io");
