a94c3f19a1aa779e1da699c1a5de34f263e3e820
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-14 23:55:07 -0700

    final commit of the night

--- controllers/thread.js
+++ controllers/thread.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models')
+const ejsLayouts = require('express-ejs-layouts')
 
 // POST /threads - create a new thread
 router.post('/', (req, res) => {
-    db.thread.create({
+    db.threads.create({
       title: req.body.title,
       content: req.body.content,
       user_name: req.body.user_name,
@@ -14,56 +15,53 @@ router.post('/', (req, res) => {
       res.redirect('/')
     })
     .catch((error) => {
-      res.status(400).render('main/404')
+    console.log(error)
     })
   })
   
   // GET /threads/new - display form for creating new threads
   router.get('/new', (req, res) => {
-    db.thread.findAll()
-    .then((threads) => {
-      res.render('threads/new', { user_id: user_id})
-    })
-    .catch((error) => {
-      res.status(400).render('main/404')
-    })
-  })
-  
-  // GET /threads/:id - display a specific thread and its comments
-  router.get('/:id', (req, res) => {
-    db.threads.findOne({
-      where: { id: req.params.id },
-      include: [db.user_id, db.comments]
-    })
-    .then((threads) => {
-      if (!threads) throw Error()
-      console.log(threads.user_id)
-      res.render('threads/show', { user_id: user_id })
-    })
-    .catch((error) => {
-      console.log(error)
-      res.status(400).render('main/404')
-    })
-  })
-  
-  // POST :3000/threads/:id/comments - route to save comment to
-  router.post("/:id/comments", async (req, res) => {
-    //get data from rec.body
-    //create new comment from data
-    //console.log new comment
-    // rerender the page so user can see comment
-    try {
-      const newComment = await db.comments.create({
-          user_name: req.body.user_name,
-          user_id: req.body.user_id,
-          content: req.body.content,
-          thread_id: req.params.id
-        })
-        res.redirect(`/threads/${req.params.id}`)
-      }catch(err){
-        console.log(err)
+    if  (res.locals.user){
+      var user_name = res.locals.user.user_name;
+        res.render('threads/new', { user: user_name})
     }
-  })
+  }) 
+  
+//   // GET /threads/:id - display a specific thread and its comments
+//   router.get('/:id', (req, res) => {
+//     db.threads.findOne({
+//       where: { id: req.params.id },
+//       include: [db.user_id, db.comments]
+//     })
+//     .then((threads) => {
+//       if (!threads) throw Error()
+//       console.log(threads.user_id)
+//       res.render('threads/show', { user_id: user_id })
+//     })
+//     .catch((error) => {
+//       console.log(error)
+//       res.status(400).render('main/404')
+//     })
+//   })
+  
+//   // POST :3000/threads/:id/comments - route to save comment to
+//   router.post("/:id/comments", async (req, res) => {
+//     //get data from rec.body
+//     //create new comment from data
+//     //console.log new comment
+//     // rerender the page so user can see comment
+//     try {
+//       const newComment = await db.comments.create({
+//           user_name: req.body.user_name,
+//           user_id: req.body.user_id,
+//           content: req.body.content,
+//           thread_id: req.params.id
+//         })
+//         res.redirect(`/threads/${req.params.id}`)
+//       }catch(err){
+//         console.log(err)
+//     }
+//   })
   
   
   
